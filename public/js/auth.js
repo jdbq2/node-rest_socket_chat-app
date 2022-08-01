dea67a26723b0c452ac989c449140724cb399cc1
Juan Diego Bohorquez Q <juan.bohorquez@vansa.co>
2022-08-01 18:48:19 -0500

    login manual agregado al index

---
+++ public/js/auth.js
@@ -0,0 +1,51 @@
+const formulario = document.querySelector("form");
+
+function handleCredentialResponse(response) {
+    const body = { id_token: response.credential };
+    fetch("http://localhost:8080/api/auth/google", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    })
+        .then((resp) => resp.json())
+        .then((resp) => {
+            console.log(resp);
+            localStorage.setItem("email", resp.usuario.correo);
+            localStorage.setItem("token", resp.token);
+        })
+        .catch((err) => console.warn(err));
+}
+
+document.getElementById("google_sign_out").addEventListener("click", () => {
+    console.log(google.accounts.id);
+    google.accounts.id.disableAutoSelect();
+    google.accounts.id.revoke(localStorage.getItem("email"), (done) => {
+        localStorage.clear();
+        location.reload();
+    });
+});
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = {};
+    for (const el of formulario.elements) {
+        if (el.name.length > 0) {
+            formData[el.name] = el.value;
+        }
+    }
+    console.log(formData);
+    fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((resp) => {
+            return resp.json();
+        })
+        .then((data) => localStorage.setItem("token", data.token))
+        .catch((err) => console.log(err));
+});
