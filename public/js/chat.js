fa93ab060cc45680dd4a2ac2dcceae05bce5af71
Juan Diego Bohorquez Q <juan.bohorquez@vansa.co>
2022-08-09 19:28:24 -0500

    modelo del chat creado al igual que el js del chat en la carpeta public

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,12 @@
 let usuario = null;
 let socket = null;
 
+const txtUid = document.getElementById("txtUid");
+const txtMensaje = document.getElementById("txtMensaje");
+const ulUsuarios = document.getElementById("ulUsuarios");
+const ulMensajes = document.getElementById("ulMensajes");
+const btnSalir = document.getElementById("btnSalir");
+
 const validarJWT = async () => {
     const token = localStorage.getItem("token");
     if (token.length <= 10) {
@@ -23,12 +29,24 @@ const validarJWT = async () => {
 };
 
 const conectarSocket = async () => {
-    const socket = io({
+    socket = io({
         extraHeaders: {
             "x-token": localStorage.getItem("token"),
         },
     });
+
+    socket.on("connect", () => {
+        console.log("Socket Online");
+    });
+    socket.on("disconnect", () => {
+        console.log("Socket Offline");
+    });
 };
+
+socket.on("recibir-mensajes", () => {});
+socket.on("usuarios-activos", () => {});
+socket.on("mensaje-privado", () => {});
+
 const main = async () => {
     await validarJWT();
 };
